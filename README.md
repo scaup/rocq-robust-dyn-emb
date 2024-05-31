a43459b033990d4094623dc9d6d943a0369897f6
Koen Jacobs <koen.jacobs@inria.fr>
2024-05-31 14:12:05 +0200

    upgrade coq + examples paper

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Create compatible opam switch
 
-latest stdpp requires coq < 8.19, so we'll be using 8.18
 
 ```
 opam switch create latest-release-iris-stdpp ocaml-base-compiler
@@ -9,5 +8,6 @@ eval $(opam env --switch=latest-release-iris-stdpp)
 opam repo add coq-released https://coq.inria.fr/opam/released
 opam repo add iris-dev https://gitlab.mpi-sws.org/iris/opam.git
 
-opam install coq.8.18.0 coq-iris.4.1.0 coq-stdpp.1.9.0 coq-iris-heap-lang.4.1.0 coq-autosubst.1.8
+opam install coq.8.19.1 coq-iris.4.2.0 coq-stdpp.1.10.0 coq-autosubst
+
 ```
---
+++ theories/logrel/examples.v
@@ -0,0 +1,70 @@
+From main.prelude Require Import imports autosubst big_op_three.
+From main.grad_lang Require Import types.
+From main.dyn_lang Require Import definition lemmas lib labels tactics.
+From main.logrel.lib Require Import weakestpre rfn small_helpers.
+From main.logrel Require Import definition.
+
+From iris.si_logic Require Export bi.
+From iris.proofmode Require Import tactics.
+(* From iris.proofmode Require Import base proofmode classes. *)
+
+
+Section examples.
+
+  Context {ν : label} {Hν : NeverOccurs ν}.
+
+  Definition sem_typed Γ e τ : Prop := open_exprel_typed Γ ⊥ e e τ.
+
+  Definition example1 : expr :=
+    Lam (
+        If ν (BinOp ν LtOp (Var 0) (BinOp ν PlusOp (Var 0) (Lit $ LitInt 1)))
+          (Var 0)
+          (App ν (Lit $ LitInt 5) (Lit $ LitBool true))
+      ).
+
+  Lemma example1_sem_typed :
+    sem_typed [] example1 (Bin Arrow (Base Int) (Base Int)).
+  Proof.
+    iIntros (Δ HΔ vs vs') "_". asimpl. rfn_val.
+    iIntros (w w') "Hww'". dvals w w'. iRewrite "Hww'". simpl.
+    rfn_steps. rewrite bool_decide_true; try by lia. rfn_val.
+  Qed.
+
+  Definition example2 : expr :=
+    LetIn (Lam (Pair (Snd ν (Var 0)) (Fst ν (Var 0)))) (* flip *)
+      (Lam (*h*) (
+           Fst ν
+             (AppAn (Var 1) (
+                  (AppAn (Var 1)
+                     (Pair
+                        (BinOp ν PlusOp (AppAn (Var 0) (Lit $ LitInt 6)) (Lit $ LitInt 1))
+                        (Lit $ LitBool true)
+                     )
+                  )
+                )
+             )
+         )
+      ).
+
+  Lemma example2_sem_typed :
+    sem_typed [] example2 (Bin Arrow (Bin Arrow (Base Int) (Base Int)) (Base Int)).
+  Proof.
+    iIntros (Δ HΔ vs vs') "_". rfn_steps. rfn_val. iNext.
+    iIntros (h h') "#Hhh'". dvals h h'. simpl. rfn_steps.
+    rfn_bind'. rw_of_val. iApply "Hhh'". auto. iNext.
+    iIntros (v v') "Hvv'". dvals v v'. iRewrite "Hvv'".
+    rfn_steps. rfn_val.
+  Qed.
+
+  Definition example3 : expr :=
+    Lam (Case ν (Var 0) (Var 1) (InjL (Lit LitUnit))).
+
+  Lemma example3_sem_typed :
+    sem_typed [] example3 (Bin Arrow (Bin Sum (Base Unit) (Base Bool))
+                             (Bin Sum (Base Unit) (Base Unit))).
+  Proof.
+    iIntros (Δ HΔ vs vs') "_". asimpl. rfn_val.
+    iIntros (s s') "#Hss'". dvals s s'; rfn_steps; rfn_val.
+  Qed.
+
+End examples.
